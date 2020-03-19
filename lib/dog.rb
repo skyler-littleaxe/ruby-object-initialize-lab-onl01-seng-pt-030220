2582ce7753fc6315a48a88e81866cc2da7e204cc
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-19 01:49:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,6 @@
 class Dog
   
-  def initialize(name)
-    @name = name
-  end
-  
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-
-  def initialize(breed)
+  def initialize(breed = "Mutt")
     @breed = breed
   end
   
